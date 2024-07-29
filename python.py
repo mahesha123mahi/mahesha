212e8057bf10cd84efb663d7ed4b49d19b89a810
mahesha123mahi <166790629+mahesha123mahi@users.noreply.github.com>
2024-07-29 01:42:48 -0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,12 @@
+print("vinu","sanju",sep="*")
+print(1-5)
+print(2+8)
+print(6/3)
+print(4*3)
+("hello")
+# my name is
+print(1.0+3.6)
+print('a'+'b'+'c')
+print(10,20,30,40,sep="\f")
+
+print("hello""hii",sep=* 
